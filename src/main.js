5c428e32389adaac60620d1c0a87995bde9f15b4
Leander Berg <reg@lulebe.net>
2017-03-05 15:53:30 +0100

    added keen-ui, vuex and vue-router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
+import KeenUI from 'keen-ui'
+import VueRouter from 'vue-router'
+
 import App from './App.vue'
+
+Vue.use(KeenUI)
+Vue.use(VueRouter)
 
 new Vue({
   el: '#app',
