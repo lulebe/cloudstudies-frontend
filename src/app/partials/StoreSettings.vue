9dd4c51cf649003555d62b0b18e1f7a677beb437
Leander Berg <reg@lulebe.net>
2017-09-18 01:04:55 +0200

    update

--- src/app/partials/StoreSettings.vue
+++ src/app/partials/StoreSettings.vue
@@ -46,24 +46,17 @@
       <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
         <input type="hidden" name="cmd" value="_s-xclick">
         <input type="hidden" name="hosted_button_id" value="3SZT35JPQR87J">
-        <table>
-          <tr><td>
-            <input type="hidden" name="on0" value="Duration">Duration
-          </td></tr>
-          <tr><td>
-            <select name="os0">
-            	<option value="1 Month">1 Month €3,00 EUR</option>
-            	<option value="3 Months">3 Months €8,00 EUR</option>
-            	<option value="6 Months">6 Months €15,00 EUR</option>
-            </select>
-          </td></tr>
-          <tr><td>
-            <input type="hidden" name="on1" value="storeId">storeId
-          </td></tr>
-          <tr><td>
-            <input type="hidden" name="os1" v-model="store.id">
-          </td></tr>
-        </table>
+        <input type="hidden" name="on0" value="Duration">
+        Duration
+        <br>
+        <select name="os0">
+        	<option value="1 Month">1 Month €3,00 EUR</option>
+        	<option value="3 Months">3 Months €8,00 EUR</option>
+        	<option value="6 Months">6 Months €15,00 EUR</option>
+        </select>
+        <br>
+        <input type="hidden" name="on1" value="storeId">
+        <input type="hidden" name="os1" v-model="store.id">
         <input type="hidden" name="currency_code" value="EUR">
         <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
         <img alt="" border="0" src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" width="1" height="1">
